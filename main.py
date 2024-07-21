3a2e6a323c1e38befc31404ab8925f8fa042e070
Akalotika <vovakosinov251@gmail.com>
2024-07-21 13:51:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-print("1st program")
-print(9**0.5*5)
-print("2nd program")
+#1st program
+print((9**0.5)*(5))
+#2nd program
 print(9.99 > 9.98 or 1000 != 1000.1)
-print("3rd program")
+#3rd program"
 print(1234 % 1004 // 10 + 5678 % 5008 // 10)
-print("4th program")
+#4th program
 print(13 == int(13.42) or 42 == int(42.13))
