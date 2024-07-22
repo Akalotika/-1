7bb52aad6b7c8c728c11861a41448b755f3c370d
Akalotika <vovakosinov251@gmail.com>
2024-07-22 20:48:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-a = "Топинамбур"
-print(a[0])
-print(a[-1])
-print(a[5::])
-print(a[::-1])
-print(a[1:10:2])
+the_number_of_completed_dz = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = 1.5 / 12
+print('Курс:' + course_name + ',всего задач:' +str(the_number_of_completed_dz)+ ', затрачено часов:' + str(number_of_hours_spent)+', среднее время выполнения ' + str(time_for_one_task))
