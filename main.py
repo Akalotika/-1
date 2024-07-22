1f692bc999b65713ba6fd17b6fcf9d745505b7d1
Akalotika <vovakosinov251@gmail.com>
2024-07-22 19:51:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-#"1st program"
-print((9**0.5)*(5))
-#"2nd program"
-print(9.99 > 9.98 or 1000 != 1000.1)
-#"3rd program"
-print(1234 % 1004 // 10 + 5678 % 5008 // 10)
-#"4th program"
-print(int(13.42) == (int(42.13 % 42.00 * 100)) or int(42.13) == (int(13.42 % 12.99 * 100)))
+a = "Топинамбур"
+print(a[0])
+print(a[-1])
+print(a[5::])
+print(a[::-1])
+print(a[1:10:2])
