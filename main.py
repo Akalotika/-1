f036a8be742011f2b44461d77a4d15c3bfbfc042
Akalotika <vovakosinov251@gmail.com>
2024-07-23 19:11:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 the_number_of_completed_dz = 12
 number_of_hours_spent = 1.5
 course_name = 'Python'
-time_for_one_task = 1.5 / 12
+time_for_one_task = number_of_hours_spent / the_number_of_completed_dz
 print('Курс:' + course_name + ',всего задач:' +str(the_number_of_completed_dz)+ ', затрачено часов:' + str(number_of_hours_spent)+', среднее время выполнения ' + str(time_for_one_task))
