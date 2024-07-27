616ead51dad56744546ff32d203b8caeb8e38656
Akalotika <vovakosinov251@gmail.com>
2024-07-27 15:24:03 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,17 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-students_ = list(students)
-students_.sort()
-a = students_.pop(0)
-j = students_.pop(1)
-s = students_.pop(2)
-b = students_.pop(-2)
-k = students_.pop(-1)
-h = [a, b, j, k, s]
-A = sum(grades.pop(0)) / 5
-K = sum(grades.pop(1)) / 4
-J = sum(grades.pop(2)) / 5
-S = sum(grades.pop(-2)) / 4
-B = sum(grades.pop(-1)) / 3
-H = [A, S, K, B, J]
-print(dict(zip(h, H)))
+grades_1 = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
+students_1 = sorted(students)
+class1 = dict(zip(students_1, grades_1))
+print(class1)
