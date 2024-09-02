e180b61e5877820f2ed71059f471ca346bbe8703
Akalotika <vovakosinov251@gmail.com>
2024-09-02 12:04:02 +0300

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -4,12 +4,11 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-        for i in range(1, new_floor + 1):
-            if 1 <= new_floor <= self.number_of_floors:
-                print(i)
-            else:
-                print("Такого этожа не существует.")
-                break
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
 
     def __len__(self):
         return self.number_of_floors
